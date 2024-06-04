5dbdd0c62d9e9c7b86da83e5b48b4c4525c12d14
Thanaphat Ketsani <65160169@go.buu.ac.th>
2024-06-04 14:37:48 +0700

    create User Object

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -15,7 +16,7 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 
-//use
+//use การเรียกใช้งาน middleware หรือ tools, framework ต่างๆที่เราติดตั้ง
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
@@ -25,7 +26,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
 
-/* FILE STORAGE */
+/* FILE STORAGE  */
 const storage = multer.diskStorage({
     destination: function (req,file,cb) {
         cb(null, "public/assets");
@@ -35,6 +36,9 @@ const storage = multer.diskStorage({
     }
 });
 const upload = multer({ storage });
+
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
 
 /* mongoose setup */
 const PORT = process.env.PORT || 6001;
