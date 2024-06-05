c7ca02b5c04aa0b887bfb909b84234ab45edd7c5
Thanaphat Ketsani <65160169@go.buu.ac.th>
2024-06-05 15:46:54 +0700

    add mock data

--- models/Post.js
+++ models/Post.js
@@ -2,33 +2,33 @@ import { Timestamp } from "mongodb";
 import mongoose from "mongoose";
 
 const postSchema = mongoose.Schema(
-    {
-        userId: {
-            type: String,
-            required: true,
-        },
-        firstName: {
-            type: String,
-            required: true,
-        },
-        lastName: {
-            type: String,
-            required: true,
-        },
-        location: String,
-        description: String,
-        picturePath: String,
-        userPicturePath: String,
-        likes: {
-            type: Map,
-            of: Boolean,
-        },
-        comments: {
-            types: Array,
-            default: []
-        }
+  {
+    userId: {
+      type: String,
+      required: true,
     },
-    { Timestamps: true }
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    location: String,
+    description: String,
+    picturePath: String,
+    userPicturePath: String,
+    likes: {
+      type: Map,
+      of: Boolean,
+    },
+    comments: {
+      type: Array,
+      default: [],
+    },
+  },
+  { Timestamps: true }
 );
 
 const Post = mongoose.model("Post", postSchema);
